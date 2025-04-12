248bcb6a745b3bbae48b63a9dc49056af3421549
hengmhs <hengmhs@gmail.com>
2025-04-12 18:11:13 +0800

    Add support for single inputs like ls

--- cmd/shellspy/main.go
+++ cmd/shellspy/main.go
@@ -3,16 +3,19 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"shellspy"
+	"strings"
 )
 
 var input string
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Please enter your full name: ")
 
 	for true {
+		fmt.Println("input: ")
 		scanner.Scan()
 		text := scanner.Text()
 		input = text
@@ -20,6 +23,39 @@ func main() {
 			break
 		}
 		fmt.Println("your input: ", text)
+		cmd, err := shellspy.CommandFromString(input)
+		// we can print out single line commands like ls
+		// but commands with args need extra work
+		// e.g. echo text or tr "a-z" "A-Z" < my_text.txt
+		// mv test.py new_test.py
+		if err != nil {
+			log.Fatal(err)
+		}
+		var out strings.Builder
+		cmd.Stdout = &out
+		cmd.Run()
+		fmt.Printf("%v", out.String())
 	}
 
 }
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os/exec"
+// 	"strings"
+// )
+
+// func main() {
+// 	cmd := exec.Command("ls")
+// 	cmd.Stdin = strings.NewReader("some input")
+// 	var out strings.Builder
+// 	cmd.Stdout = &out
+// 	err := cmd.Run()
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	fmt.Printf("in all caps: %q\n", out.String())
+// }
--- shellspy.go
+++ shellspy.go
@@ -8,6 +8,9 @@ import (
 func CommandFromString(input string) (*exec.Cmd, error) {
 	cmd := exec.Command(input)
 	return cmd, nil
+	// how do I do error handling for exec.Command?
+	// e.g. if the input is not a terminal command
+	// https://pkg.go.dev/os/exec#Command
 }
 
 func ReadInputLoop(input io.Reader) {
